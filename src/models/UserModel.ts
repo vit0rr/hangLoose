488c7fb511d914065f2efec983168ff399fca7a2
Vitor <vi.souza.almeida@protonmail.com>
2023-03-21 18:02:34 -0300

    feat: crate user

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -8,17 +8,23 @@ const Schema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
     },
     avatar: {
         type: String,
         required: false,
     },
+    hasHangloose: {
+        type: Boolean,
+        required: false,
+    }
 })
 
 export interface IUser extends mongoose.Document {
     name: string;
     email: string;
     avatar?: string;
+    hasHangloose?: boolean;
 }
 
 const UserModel = mongoose.model<IUser>('User', Schema);
