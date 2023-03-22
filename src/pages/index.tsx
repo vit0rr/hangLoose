e858d736cf498e05073aa2d0fc4e49a6ca4b4687
Vitor <vi.souza.almeida@protonmail.com>
2023-03-22 00:23:01 -0300

    feat: add callback to create user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import LoginButton from '@/components/LoginButton/LoginButton'
 import { useSession } from 'next-auth/react'
-
-
 
 export default function Home() {
   const { data: session } = useSession()
