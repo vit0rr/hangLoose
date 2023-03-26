f161a734679d08cda0ded05e4582ba62b916bcc8
Vitor <vi.souza.almeida@protonmail.com>
2023-03-26 18:08:06 -0300

    feat: fix parse github

--- src/utils/getGithubId.ts
+++ src/utils/getGithubId.ts
@@ -2,9 +2,7 @@ export const getGithubId = (avatar: string) => {
     const parts = avatar.split('/');
     const idIndex = parts.indexOf('u') + 1;
     if (idIndex > 0 && idIndex < parts.length) {
-      const userId = parseInt(parts[idIndex]);
-      return (userId - 1).toString();
-    }
+      return parseInt(parts[idIndex]);    }
     throw new Error('Invalid avatar URL');
   };
   
