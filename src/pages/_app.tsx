d14caee6b50b11df2f50c45774b35809d49c839c
Vitor <vi.souza.almeida@protonmail.com>
2023-03-27 12:11:25 -0300

    feat: add description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,32 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { SessionProvider } from 'next-auth/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="HangLoose! It's a new way to connect with other developers. Check it out!" />
+        <title>HangLoose</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
 }
+
+/* <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html> */
