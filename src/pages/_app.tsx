052324a250adbb303310f7432a6667c7750e1e6e
Vitor <vi.souza.almeida@protonmail.com>
2023-03-27 12:12:58 -0300

    feat: remove unused comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,16 +19,3 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     </SessionProvider>
   )
 }
-
-/* <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html> */
