3c3c2da619348fc14e9e209a49c2cc3502159f2b
vitor <vi.souza.almeida@protonmail.com>
2023-03-27 12:13:38 -0300

    Feat/tweet button (#8)
    * feat: add tweet button
    
    * feat: add description
    
    * feat: remove unused comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,18 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { SessionProvider } from 'next-auth/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="HangLoose! It's a new way to connect with other developers. Check it out!" />
+        <title>HangLoose</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
