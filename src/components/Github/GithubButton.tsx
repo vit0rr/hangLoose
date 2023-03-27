5638605990662352094e41d77107b14ecb449bec
Vitor <vi.souza.almeida@protonmail.com>
2023-03-27 12:33:17 -0300

    feat: add github link

---
+++ src/components/Github/GithubButton.tsx
@@ -0,0 +1,7 @@
+import styles from '@/styles/home.module.css'
+
+export default function GithubButton() {
+    return (
+        <a href="https://github.com/vit0rr/hangLoose" target="_blank" className={styles.githubLink}>Check it at Github!</a>
+    )
+}
