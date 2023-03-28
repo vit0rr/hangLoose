20005660c66e752231cd394b38b216d31bf49304
Vitor <vi.souza.almeida@protonmail.com>
2023-03-28 17:56:16 -0300

    style(ui): remove unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
-import styles from '@/styles/hangloose.module.css'
 import '@/styles/globals.css'
 
 import type { AppProps } from 'next/app'
-
 import { SessionProvider } from 'next-auth/react'
 import Head from 'next/head'
-import GithubButton from '@/components/Github/GithubButton'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
